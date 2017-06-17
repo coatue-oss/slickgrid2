59a03b75c31ebb2fed3501b52f66861661110b7c
Chris Khoo <chris.khoo@gmail.com>
2017-06-16 19:20:17 -0700

    generate examples so can easily change overall layout if need be

---
+++ examples/src/generate-files.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const glob = require('glob')
+
+function template(text) {
+return `<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="../dist/slickgrid2.css">
+  <link rel="stylesheet" href="slick-default-theme.css">
+  <link rel="stylesheet" href="examples.css">
+  <script src="../node_modules/lodash/lodash.js" defer></script>
+  <script src="../node_modules/jquery/dist/jquery.js" defer></script>
+  <script src="../node_modules/@bcherny/jquery.threedubmedia/event.drag/jquery.event.drag.js" defer></script>
+  <script src="../dist/slickgrid2.js" defer></script>
+</head>
+<body>
+${text}
+</body>
+</html>
+`
+}
+
+function openTemplateFile(filename) {
+  return fs.readFileSync(path.resolve(__dirname, filename))
+}
+
+function saveTemplateFile(filename, text) {
+  return fs.writeFileSync(path.resolve(__dirname, '..', filename), text)
+}
+
+glob.sync(path.resolve(__dirname, '*.html'))
+.map(filePath => path.basename(filePath))
+.forEach(filename => {
+  const text = openTemplateFile(filename)
+  saveTemplateFile(filename, template(text))
+  console.log(`${filename} done.`)
+})
+console.log('All done.')
